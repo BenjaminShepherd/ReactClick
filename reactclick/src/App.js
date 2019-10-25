ef3c5b0eed529f78139cb8ab72bcf079ab508f4a
BenShepherd <19ute14@gmail.com>
2019-10-25 14:24:02 -0600

    added state to app

--- reactclick/src/App.js
+++ reactclick/src/App.js
@@ -1,7 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
+import TodoList from './TodoList'
 
 
 function App() {
-  return null
+  return (
+    <>
+      <TodoList />
+      <input type="text" />
+      <button>Add Todo</button>
+      <button>Clear Completed</button>
+      <div>0 Left To Do</div>
+    </>
+  )
 }
 export default App;
