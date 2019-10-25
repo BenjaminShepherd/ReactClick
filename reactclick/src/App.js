56b280e4f1c398d2e15714931205670b8cf736fb
BenShepherd <19ute14@gmail.com>
2019-10-25 16:24:50 -0600

    working logic

--- reactclick/src/App.js
+++ reactclick/src/App.js
@@ -34,13 +34,18 @@ function App() {
     todoNameRef.current.value = null
 
   }
+
+  function handleClearTodos() {
+    const newTodos = todos.filter(todo => !todo.complete)
+    setTodos(newTodos)
+  }
   return (
     <>
       <TodoList todos={todos} toggleTodo={toggleTodo} />
       <input ref={todoNameRef} type="text" />
-      <button onClick={handleAddTodo}>Add Todo</button>
-      <button>Clear Completed</button>
-      <div>0 Left To Do</div>
+      <button onKeyPress={handleAddTodo} onClick={handleAddTodo}>Add Todo</button>
+      <button onClick={handleClearTodos}      > Clear Completed</button>
+      <div>{todos.filter(todo => !todo.complete).length} Left To Do</div>
     </>
   )
 }
