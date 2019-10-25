92556c47cad4c84da7b9d23231de1e2b9f38fc12
BenShepherd <19ute14@gmail.com>
2019-10-25 15:27:44 -0600

    added map of todos

--- reactclick/src/App.js
+++ reactclick/src/App.js
@@ -3,9 +3,10 @@ import TodoList from './TodoList'
 
 
 function App() {
+  const [todos, setTodos] = useState(['Todo 1', 'Todo 2'])
   return (
     <>
-      <TodoList />
+      <TodoList todos={todos} />
       <input type="text" />
       <button>Add Todo</button>
       <button>Clear Completed</button>
